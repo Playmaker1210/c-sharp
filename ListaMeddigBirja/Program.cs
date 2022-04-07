baf73a91fe8e0aa4725d7b9e2df0a7dc6bf98b07
Playmaker1210 <hatvanitamas09@gmail.com>
2022-04-07 18:31:07 +0200

    update

---
+++ ListaMeddigBirja/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+namespace ListaMeddigBirja {
+    internal class Program {
+        static void Main(string[] args) {
+
+            List<byte> szamok1 = new List<byte>();
+
+            Console.WriteLine("Milyen hosszú legyen a lista?");
+            
+            Random random = new Random();
+            Stopwatch timer = new Stopwatch();
+
+            //int a = Convert.ToInt32(Console.ReadLine());
+
+            timer.Start();
+
+            uint i = 0;
+            try {
+                for (i = 0; i < Int32.MaxValue; i++) {
+                    szamok1.Add(Convert.ToByte(random.Next(1, 10)));
+                }
+            } catch (OutOfMemoryException e) {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(i);
+                timer.Stop();
+                while (true);               
+            }
+
+            /*for (int i = 0; i < a; i++) {
+                Console.WriteLine("{0}/{1}", szamok[i], i);
+            }*/
+
+            timer.Stop();
+            StreamWriter writer = new StreamWriter("eltelt_ido.txt");
+            writer.WriteLine("ido: " + timer.Elapsed);
+            writer.Close();
+            Console.WriteLine("ido: " + timer.Elapsed);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ MatrixBill/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixBill {
+    internal class Program {
+        public static void kiir(int[,] tomb) {
+            for(int i = 0; i < tomb.GetLength(0); i++) {
+                for(int j = 0; j < tomb.GetLength(1); j++) {
+                    Console.WriteLine(tomb[i,j]);
+                }
+            }
+        }
+
+        static void Main(string[] args) {
+            int sor = Convert.ToInt32(Console.ReadLine());
+            int oszlop = Convert.ToInt32(Console.ReadLine());
+
+            int[,] tomb = new int[sor, oszlop];
+            double bekeres = 0;
+
+            for (int i = 0; i < tomb.GetLength(0); i++) {
+                for(int j = 0; j < tomb.GetLength(1); j++) {
+                    Console.WriteLine("Adja meg a kovetkezo elemet");
+                    bekeres = Convert.ToDouble(Console.ReadLine());
+                    tomb[i,j] = Convert.ToInt32(Math.Round(bekeres));
+                }
+            }
+
+            kiir(tomb);
+
+            Console.ReadKey();
+        }
+    }
+}
