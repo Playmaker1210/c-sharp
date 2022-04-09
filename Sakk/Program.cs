b5b7a1818007b92505640cf39c47a0d8ab9d8fdc
thewarrior1220 <hatvanitamas09@gmail.com>
2022-04-09 16:38:03 +0200

    update

--- Sakk/Program.cs
+++ Sakk/Program.cs
@@ -24,15 +24,56 @@ namespace Sakk {
             return true; //ha nem talalt sehol se babut akkor return true
         }
 
-        static void Main(string[] args) {
-            int lehetosegek = 0;
-
+        public static void kiir() {
             for (int i = 0; i < tabla.GetLength(0); i++) {
                 for (int j = 0; j < tabla.GetLength(1); j++) {
-
+                    Console.Write(tabla[i,j] + " ");
                 }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args) {
+            //problema ha el van tolva a sor vagy az oszlop nem rak le 8 babut
+            //csak egy fajta variaciot tud megcsinalni
+            Random random = new Random();
+            List<int[,]> tablak = new List<int[,]>();
+            //int lehetosegek = 0;
+            int babuk = 0;
+            int akt_sor = 0;
+            bool nem_jo_ketszer = false;
+            int elozo_poz = 0, elozo_sor = 0;
+
+            while(babuk < 9) {
+                int pozicio = random.Next(0, 7);
+                Console.WriteLine("pozicio: " + pozicio);
+                Console.WriteLine("akt sor:" + akt_sor);
+                if(sorEllenorzes(akt_sor) && oszlopEllenorzes(pozicio)) {
+                    tabla[akt_sor, pozicio] = 1;
+                    elozo_poz = pozicio;    //elozo babu elmentese
+                    elozo_sor = akt_sor;
+                    babuk++;
+                    akt_sor++;
+                }
+
+                if(elozo_sor == akt_sor) {
+                    nem_jo_ketszer = true;
+                    if (nem_jo_ketszer) {
+                        tabla = new int[8, 8];
+                        babuk = 0,
+                    }
+                }
+
+                //atnezni van e mar ez a tabla eltarolva
+                if(babuk == 8) {
+                    tablak.Add(tabla);
+                }
+
+                kiir();
             }
             
+
+           
 
             Console.ReadKey();
         }
