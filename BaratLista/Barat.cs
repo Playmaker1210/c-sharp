4ebeab9e533ea640730238847577372b58922063
thewarrior1220 <hatvanitamas09@gmail.com>
2022-05-18 17:56:52 +0200

    auto lista

---
+++ BaratLista/Barat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaratLista {
+    public class Barat {
+        private string nev;
+        private DateTime szuletes_ido;
+        private string nem;
+        private int bulizasi_szam;
+
+        public Barat(string nev, DateTime szul_ido, string nem, int b_szam) {
+            this.nev = nev;
+            this.nem = nem;
+            this.szuletes_ido = szul_ido;
+            this.bulizasi_szam = b_szam;
+        }
+
+        public void kiir() {
+            string ido = szuletes_ido.ToShortDateString();
+            Console.WriteLine("Név: " + nev + " Születési idő: " + ido + " Nem: " + nem + " Bulizasi szam: " + bulizasi_szam);
+        }
+    }
+}
---
+++ BaratLista/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BaratLista {
+    public class Program {
+            
+        public static void beolvas() {
+
+        }
+
+        static void Main(string[] args) {
+            List<Barat> baratok = new List<Barat>();
+
+            StreamReader reader = new StreamReader("baratok.txt", Encoding.Default);
+            int c = 0;
+            while(!reader.EndOfStream) {
+                if (c % 100000 == 0) Console.WriteLine(c);
+                c++;
+                string input = reader.ReadLine();
+                string[] adatok = input.Split(',');
+                baratok.Add(new Barat(adatok[0], DateTime.Parse(adatok[1]), adatok[2], Convert.ToInt32(adatok[3])));
+            }
+
+            for (int i = 0; i < baratok.Count; i++) {
+                if (c % 100000 == 0)
+                    baratok[i].kiir();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
