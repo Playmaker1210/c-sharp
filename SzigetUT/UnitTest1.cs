c958775e1b3cd76444898b8e19f87bc175694e81
thewarrior1220 <hatvanitamas09@gmail.com>
2022-06-09 09:54:49 +0200

    update

---
+++ SzigetUT/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Szigetek;
+using System.IO;
+
+namespace SzigetUT {
+    [TestClass]
+    public class UnitTest1 {
+
+        [TestMethod]
+        public void testSzam() {
+            StringReader sr = new StringReader("10101110");
+            Console.SetIn(sr);
+            Sziget.setSziget();          
+            Assert.AreEqual(Sziget.vizsgalat()[0], 5);
+        }
+
+        [TestMethod]
+        public void testHossz() {
+            StringReader sr = new StringReader("10101110");
+            Console.SetIn(sr);
+            Sziget.setSziget();
+            Assert.AreEqual(Sziget.vizsgalat()[1], 3);
+        }
+    }
+}
---
+++ Szigetek/Sziget.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szigetek {
+    public static class Sziget {
+        private static int sziget_sorozat = 0;
+        private static int sziget_sorozat_max = 0;
+        private static int sziget_sum = 0;
+        private static string sziget = "";
+        private static bool sorozat = false;
+
+        public static void setSziget() {
+            Console.WriteLine("Adja meg a szigeteket | sziget 1, tenger 0");
+            sziget = Console.ReadLine();
+        }
+
+        public static int[] vizsgalat() {
+            for (int i = 0; i < sziget.Length; i++) {
+                if (sziget[i].Equals('1')) {
+                    sziget_sum++;
+                    sorozat = true;
+                    if (sorozat) {
+                        sziget_sorozat++;
+                    }
+                } else {
+                    sorozat = false;
+                    sziget_sorozat_max = sziget_sorozat;
+                    sziget_sorozat = 0;
+                }
+
+                if (i == sziget.Length-1) {
+                    if (sziget_sorozat_max < sziget_sorozat) {
+                        sziget_sorozat_max = sziget_sorozat;
+                    }
+                }
+            }
+
+            int[] tomb = { sziget_sum, sziget_sorozat_max };
+            return tomb;
+        }
+
+    }
+}
